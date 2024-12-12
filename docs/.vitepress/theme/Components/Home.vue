328a97ed42a20f7e2dd9d26874d2cf80b86a2252
xygodcyx <109750146+xygodcyx@users.noreply.github.com>
2024-12-12 22:33:09 +0800

    try fix Hydration completed but contains mismatches

--- docs/.vitepress/theme/Components/Home.vue
+++ docs/.vitepress/theme/Components/Home.vue
@@ -13,20 +13,11 @@ onMounted(() => {
 
 <template>
   <div class="w-100vw flex-col items-center">
-    <!-- <h1 class="text-center mb-4">所有文章</h1> -->
     <ul class="list-none flex flex-col items-center w-full">
       <li v-for="post of posts"
         class="w-90% mb-2 md:w-50%">
         <Article :post="post" />
       </li>
     </ul>
-    <!-- <ul class="fixed right-10% top-10% mb-4 md:mb-0">
-      <li v-for="post of posts">
-        <a :href="'#' + post.title"
-          class="text-gray-500 hover:text-gray-900"
-          :class="{ 'text-gray-900': hash === encodeURI(post.title) }">
-          {{ post.title }}</a>
-      </li>
-    </ul> -->
   </div>
 </template>
--- docs/.vitepress/theme/Components/TopNav.vue
+++ docs/.vitepress/theme/Components/TopNav.vue
@@ -21,34 +21,6 @@
         title="force76246"
         href="https://x.com/force76246">X</TextIcon>
     </div>
-    <!-- <div class="icons flex gap-1 items-center"
-        v-if="false">
-        <a target="_blank"
-          title="Bilibili"
-          href="https://space.bilibili.com/1308065752">
-          <div class="i-tabler:brand-bilibili"></div>
-        </a>
-        <a target="_blank"
-          title="Github"
-          href="https://github.com/xygodcyx/">
-          <div class="i-tabler:brand-github"></div>
-        </a>
-        <a target="_blank"
-          title="YouTube"
-          href="https://www.youtube.com/channel/UCFvMt-qiO3YGKAkbhB25vxg">
-          <div class="i-tabler:brand-youtube"></div>
-        </a>
-        <a target="_blank"
-          title="X"
-          href="https://x.com/force76246">
-          <div class="i-tabler:brand-x"></div>
-        </a>
-        <a target="_blank"
-          title="RSS"
-          href="javascript:void(0)">
-          <div class="i-tabler:rss"></div>
-        </a>
-      </div> -->
   </nav>
 </template>
 
