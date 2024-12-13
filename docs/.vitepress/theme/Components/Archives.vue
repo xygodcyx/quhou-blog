e5fddfe6503f435ef047a8727208542aacb3a12d
xygodcyx <109750146+xygodcyx@users.noreply.github.com>
2024-12-13 22:13:21 +0800

    show archives year

--- docs/.vitepress/theme/Components/Archives.vue
+++ docs/.vitepress/theme/Components/Archives.vue
@@ -8,7 +8,7 @@ import { data as articles } from '../data/archive.data'
     <ul class="list-none flex flex-col items-center gap-10 w-full md:w-80% lg:w-70% xl:w-60% ">
       <li v-for="article of articles"
         class="text-center w-full flex flex-col items-center relative h-fit">
-        <h1 class="z--999 text-30 opacity-10 absolute left-0 bottom-50% transform-translate-y-40%">{{ article.year }}
+        <h1 class="text-20 opacity-10 absolute left-50% bottom-50% transform-translate-y-40% transform-translate-x--80%">{{ article.year }}
         </h1>
         <ol class="flex flex-col items-start w-full gap-2">
           <li v-for="post of article.posts"
